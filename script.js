23606a054077bcbe258ec0ee756bd8654ce2f42c
Atila Guilherme <atilacarrao@hotmail.com>
2022-07-26 16:51:41 -0300

    iconAdd

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuId = document.getElementById("menuId");
+
 const navOn = ()=>{
     const nav = document.getElementById("nav");
     nav.classList.toggle("active");
 };
 menuId.addEventListener("click", navOn);
-
